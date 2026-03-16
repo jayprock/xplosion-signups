96f6bd32bab9eebcc66f129ef2a43e8c5f6ec39f
Joe Prochnow <jprochnow1@gmail.com>
2026-03-16 19:10:09 -0400

    Add roster to team page

--- src/app/teams/new/actions.ts
+++ src/app/teams/new/actions.ts
@@ -1,20 +1,26 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { createTeam, generateTeamSlug } from "@/lib/data";
+import { createTeam, generateTeamSlug, addPlayer } from "@/lib/data";
 
 export async function createTeamAction(data: {
   name: string;
   coachLastName: string;
   seasonYear: number;
   adminPassword: string;
+  playerNames?: string[];
 }): Promise<{ teamSlug: string }> {
   const slug = await generateTeamSlug(data.coachLastName);
 
+  const { playerNames, ...teamData } = data;
   const team = await createTeam({
-    ...data,
+    ...teamData,
     slug,
   });
+
+  if (playerNames && playerNames.length > 0) {
+    await Promise.all(playerNames.map((name) => addPlayer(team.id, name)));
+  }
 
   revalidatePath("/");
   return { teamSlug: team.slug };
